9429a3b259a3e8b78fb4ee76ec21f5d6b332b972
Natsu-Akatsuki <1024756993@qq.com>
2024-11-30 17:39:01 +0800

    Add and rename action. Update Dockerfile.

--- docker/generate_dockerfile.py
+++ docker/generate_dockerfile.py
@@ -1,36 +1,31 @@
-"""
-Refer to
-1. https://github.com/pytorch/TensorRT/blob/main/docker/Dockerfile
-"""
-
-from pathlib import Path
+import argparse
 import os
-
-# {22.04, 20.04}
-UBUNTU_VERSION = "22.04"
-# {noetic, humble}
-ROS_VERSION = "humble"
-TENSORRT_VERSION = "10.6"
-# {11.1.1, 12.4.1 (require: nvidia-driver 550)}
-CUDA_VERSION = "12.4.1"
-
-target_dir = Path(__file__).resolve().parent
-prefix = f"ubuntu{UBUNTU_VERSION}_cuda{CUDA_VERSION}_tensorrt{TENSORRT_VERSION}_{ROS_VERSION}"
-target_dir = target_dir / prefix
-target_dir.mkdir(parents=True, exist_ok=True)
+from pathlib import Path
 
 
-def set_basic_image():
-    if CUDA_VERSION == "11.1.1":
-        return f"nvidia/cuda:{CUDA_VERSION}-cudnn8-devel-ubuntu{UBUNTU_VERSION}"
-    else:
-        return f"nvidia/cuda:{CUDA_VERSION}-cudnn-devel-ubuntu{UBUNTU_VERSION}"
+def parse_args():
+    parser = argparse.ArgumentParser(description="Generate Dockerfile and related files.")
+    parser.add_argument("--ubuntu_version", default="22.04", choices=["22.04", "20.04"], help="Ubuntu version (default: 22.04)")
+    parser.add_argument("--ros_version", default="humble", choices=["noetic", "humble"], help="ROS version (default: humble)")
+    parser.add_argument("--tensorrt_version", default="10.6", help="TensorRT version (default: 10.6)")
+    parser.add_argument("--cuda_version", default="12.4.1", help="CUDA version (default: 12.4.1)")
+
+    return parser.parse_args()
 
 
-def set_ros():
-    # Note: the default bash is /bin/sh, thus we do not use "source"
-    tmp_dict = {
-        "noetic": """
+def generate_dockerfile(ubuntu_version, ros_version, tensorrt_version, cuda_version, target_dir):
+    target_dir.mkdir(parents=True, exist_ok=True)
+
+    def set_basic_image():
+        if cuda_version == "11.1.1":
+            return f"nvidia/cuda:{cuda_version}-cudnn8-devel-ubuntu{ubuntu_version}"
+        else:
+            return f"nvidia/cuda:{cuda_version}-cudnn-devel-ubuntu{ubuntu_version}"
+
+    def set_ros():
+        # Note: the default bash is /bin/sh, thus we do not use "source"
+        tmp_dict = {
+            "noetic": """
 RUN DEBIAN_FRONTEND=noninteractive sh -c '. /etc/lsb-release && echo "deb http://mirrors.tuna.tsinghua.edu.cn/ros/ubuntu/ `lsb_release -cs` main" > /etc/apt/sources.list.d/ros-latest.list' \\
     && apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654 \\
     && apt update \\
@@ -42,19 +37,17 @@ RUN DEBIAN_FRONTEND=noninteractive sh -c '. /etc/lsb-release && echo "deb http:/
         python3-wstool \\
         python3-pip \\
     && rm -rf /var/lib/apt/lists/*""",
-        "humble": """
+            "humble": """
 RUN curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg \\
     && echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | tee /etc/apt/sources.list.d/ros2.list > /dev/null \\
     && apt update \\
-    && DEBIAN_FRONTEND=noninteractive apt install -y ros-humble-desktop
+    && DEBIAN_FRONTEND=noninteractive apt install -y ros-humble-desktop python3-colcon-common-extensions
 """
-    }
+        }
+        return tmp_dict[ros_version]
 
-    return tmp_dict[ROS_VERSION]
-
-
-def set_apt():
-    return """
+    def set_apt():
+        return """
 RUN sed -i s@/archive.ubuntu.com/@/mirrors.tuna.tsinghua.edu.cn/@g /etc/apt/sources.list \\
     && rm /etc/apt/sources.list.d/* \\
     && apt update \\
@@ -89,9 +82,8 @@ RUN sed -i s@/archive.ubuntu.com/@/mirrors.tuna.tsinghua.edu.cn/@g /etc/apt/sour
     && rm -rf /var/lib/apt/lists/*
 """
 
-
-def set_nvidia():
-    return """
+    def set_nvidia():
+        return """
 # >>> nvidia-container-runtime >>>
 ENV NVIDIA_VISIBLE_DEVICES ${NVIDIA_VISIBLE_DEVICES:-all}
 ENV NVIDIA_DRIVER_CAPABILITIES ${NVIDIA_DRIVER_CAPABILITIES:+$NVIDIA_DRIVER_CAPABILITIES,}graphics
@@ -99,24 +91,20 @@ ENV __NV_PRIME_RENDER_OFFLOAD=1
 ENV __GLX_VENDOR_LIBRARY_NAME=nvidia
 """
 
-
-def set_tensort():
-    tmp_str = """
+    def set_tensort():
+        tmp_str = """
 # >>> Install TensorRT >>>
 """
-
-    tmp_str += f"ENV TENSORRT_VERSION {TENSORRT_VERSION}\n"
-
-    if UBUNTU_VERSION == "20.04":
-        tmp_str += """RUN apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/7fa2af80.pub \\
+        tmp_str += f"ENV TENSORRT_VERSION {tensorrt_version}\n"
+        if ubuntu_version == "20.04":
+            tmp_str += """RUN apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/7fa2af80.pub \\
     && add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/ /" \\
 """
-    if UBUNTU_VERSION == "22.04":
-        tmp_str += """RUN apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/3bf863cc.pub \\
+        if ubuntu_version == "22.04":
+            tmp_str += """RUN apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/3bf863cc.pub \\
     && add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/ /" \\
 """
-
-    tmp_str += """    && apt update \\
+        tmp_str += """    && apt update \\
     && TENSORRT_MAJOR_VERSION=`echo ${TENSORRT_VERSION} | cut -d '.' -f 1` \\
     && DEBIAN_FRONTEND=noninteractive apt install -y \\
         libnvinfer${TENSORRT_MAJOR_VERSION}=${TENSORRT_VERSION}.* \\
@@ -129,66 +117,58 @@ def set_tensort():
         libnvonnxparsers-dev=${TENSORRT_VERSION}.* \\
     && rm -rf /var/lib/apt/lists/*
 """
-    return tmp_str
+        return tmp_str
 
-
-def set_shell():
-    return """
+    def set_shell():
+        return """
 SHELL ["/bin/bash", "-c"]
 """
 
-
-def set_others():
-    tmp_str = ""
-    if UBUNTU_VERSION == "20.04":
-        tmp_str += """# Fix https://github.com/ros-visualization/rviz/issues/1780.
+    def set_others():
+        tmp_str = ""
+        if ubuntu_version == "20.04":
+            tmp_str += """# Fix https://github.com/ros-visualization/rviz/issues/1780.
 RUN DEBIAN_FRONTEND=noninteractive add-apt-repository ppa:beineri/opt-qt-5.12.10-focal -y \\
     && apt update \\
     && apt install -y qt512charts-no-lgpl qt512svg qt512xmlpatterns qt512tools qt512translations qt512graphicaleffects qt512quickcontrols2 qt512wayland qt512websockets qt512serialbus qt512serialport qt512location qt512imageformats qt512script qt512scxml qt512gamepad qt5123d
     
 """
-    tmp_str += """# Install hstr
+        tmp_str += """# Install hstr
 RUN DEBIAN_FRONTEND=noninteractive add-apt-repository ppa:ultradvorka/ppa -y \\
     && apt update \\
     && apt update && apt install -y hstr \\
     && rm -rf /var/lib/apt/lists/*
 """
-    return tmp_str
+        return tmp_str
 
-
-def set_entrypoint():
-    return """
+    def set_entrypoint():
+        return """
 ENTRYPOINT ["/bin/bash"]
 """
 
-
-def set_user_permissions():
-    return """# >>> Change user permissions >>>
+    def set_user_permissions():
+        return """# >>> Change user permissions >>>
 ARG USER_NAME=rangenet
 # Set the password '123' for the 'rangenet' user
 RUN useradd ${USER_NAME} -m -G sudo -u 1000 -s /bin/bash && echo ${USER_NAME}:123 | chpasswd
 USER ${USER_NAME}
 """
 
+    def set_workdir():
+        return """WORKDIR /home/${USER_NAME}"""
 
-def set_workdir():
-    return """WORKDIR /home/${USER_NAME}"""
+    def set_bashrc():
+        tmp_str = ""
+        if ros_version == "noetic":
+            tmp_str += 'RUN echo "source /opt/qt512/bin/qt512-env.sh" >> ~/.bashrc'
 
-
-def set_bashrc():
-    tmp_str = ""
-    if ROS_VERSION == "noetic":
-        tmp_str += 'RUN echo "source /opt/qt512/bin/qt512-env.sh" >> ~/.bashrc'
-
-    tmp_str += f"""
-RUN echo "source /opt/ros/{ROS_VERSION}/setup.bash" >> ~/.bashrc
+        tmp_str += f"""
+RUN echo "source /opt/ros/{ros_version}/setup.bash" >> ~/.bashrc
 """
+        return tmp_str
 
-    return tmp_str
-
-
-def download_libtorch():
-    return """# >>> Download libtorch >>>
+    def download_libtorch():
+        return """# >>> Download libtorch >>>
 RUN cd ~ \\
     && wget -c https://download.pytorch.org/libtorch/cu111/libtorch-cxx11-abi-shared-with-deps-1.10.0%2Bcu111.zip \\
     && unzip libtorch-cxx11-abi-shared-with-deps-1.10.0+cu111.zip \\
@@ -196,14 +176,11 @@ RUN cd ~ \\
     && echo "export Torch_DIR=${HOME}/libtorch/share/cmake/Torch" >> ~/.bashrc
     """
 
-
-def setup_workspace():
-    return """
+    def setup_workspace():
+        return """
 RUN git clone https://github.com/Natsu-Akatsuki/RangeNet-TensorRT ~/rangenet/rangenet/src
 """
 
-
-def generate_dockerfile():
     target_path = target_dir / "Dockerfile"
     with open(target_path, "w") as f:
         f.write(f"FROM {set_basic_image()}\n")
@@ -221,9 +198,8 @@ def generate_dockerfile():
         # f.write(f"{setup_workspace()}\n")
 
 
-def set_docker_compose():
-    user_home = os.getenv("HOME")
-    tmp_str = f"""services:
+def set_docker_compose(user_home, prefix):
+    return f"""services:
   liobench:
     image: rangenet:{prefix}
     container_name: rangenet
@@ -247,30 +223,52 @@ def set_docker_compose():
     stdin_open: true
     restart: 'no'
 """
-    return tmp_str
 
 
-def generate_docker_compose():
+def generate_docker_compose(target_dir, user_home, prefix):
     target_path = target_dir / "docker-compose.yml"
     with open(target_path, "w") as f:
-        f.write(f"{set_docker_compose()}")
+        f.write(f"{set_docker_compose(user_home, prefix)}")
 
 
-def set_build_bash():
-    tmp_str = f"""#!/bin/bash
+def set_build_bash(prefix):
+    return f"""#!/bin/bash
 docker build -t rangenet:{prefix} --network=host -f Dockerfile .
 """
 
-    return tmp_str
 
-
-def generate_build_bash():
+def generate_build_bash(target_dir, prefix):
     target_path = target_dir / "build_image.sh"
     with open(target_path, "w") as f:
-        f.write(f"{set_build_bash()}")
+        f.write(f"{set_build_bash(prefix)}")
+
+
+def main():
+    args = parse_args()
+
+    if 1:
+        # {22.04, 20.04}
+        ubuntu_version = "20.04"
+        # {noetic, humble}
+        ros_version = "noetic"
+        tensorrt_version = "10.6"
+        # {11.1.1, 12.4.1 (require: nvidia-driver 550)}
+        cuda_version = "12.4.1"
+    else:
+        ubuntu_version = args.ubuntu_version
+        ros_version = args.ros_version
+        tensorrt_version = args.tensorrt_version
+        cuda_version = args.cuda_version
+
+    target_dir = Path(__file__).resolve().parent
+    prefix = f"ubuntu{ubuntu_version}_cuda{cuda_version}_tensorrt{tensorrt_version}_{ros_version}"
+    target_dir = target_dir / prefix
+
+    generate_dockerfile(ubuntu_version, ros_version, tensorrt_version, cuda_version, target_dir)
+    user_home = os.getenv("HOME")
+    generate_docker_compose(target_dir, user_home, prefix)
+    generate_build_bash(target_dir, prefix)
 
 
 if __name__ == "__main__":
-    generate_dockerfile()
-    generate_docker_compose()
-    generate_build_bash()
+    main()
