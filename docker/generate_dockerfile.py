eca9bfacd76cf5c11911f42bc4d5df533459dc7a
Natsu-Akatsuki <1024756993@qq.com>
2024-12-01 19:25:27 +0800

    Fix missing dependency in humble image

--- docker/generate_dockerfile.py
+++ docker/generate_dockerfile.py
@@ -41,7 +41,7 @@ RUN DEBIAN_FRONTEND=noninteractive sh -c '. /etc/lsb-release && echo "deb http:/
 RUN curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg \\
     && echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | tee /etc/apt/sources.list.d/ros2.list > /dev/null \\
     && apt update \\
-    && DEBIAN_FRONTEND=noninteractive apt install -y ros-humble-desktop python3-colcon-common-extensions
+    && DEBIAN_FRONTEND=noninteractive apt install -y ros-humble-desktop python3-colcon-common-extensions ros-humble-pcl-ros
 """
         }
         return tmp_dict[ros_version]
